990c41f101aec80c5b7d9fb3ca3d47fc30919b94
collinshen123 <collinshen123@gmail.com>
2023-12-30 20:57:17 -0800

    finished chat feature

--- chat.py
+++ chat.py
@@ -23,7 +23,7 @@ model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
-bot_name = "Sam"
+bot_name = "Bob"
 print("Let's chat! type 'quit' to exit")
 while True:
     sentance = input('You: ')
